0c26c09efea212639174ea69c818b4009f9967c7
JorgeMtz <geokg.j@gmail.com>
2022-11-09 01:43:36 -0600

    update placeholder

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -35,7 +35,7 @@ const UserSearch = () => {
               <input
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
-                placeholder="Search"
+                placeholder="Search Github users"
                 value={text}
                 onChange={handleChange}
               />
